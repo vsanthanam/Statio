f3c7e319b0f41276e575fa7ef807e49e59bb2001
Varun Santhanam <talkto@vsanthanam.com>
2021-10-02 09:18:19 -0700

    [CPU 11/x] Add Snapshot Tests
    Summary: Add close tap handler, snapshot tests to `ProcessorViewController`
    
    Test Plan: n/a
    
    Reviewers: vsanthanam
    
    Reviewed By: vsanthanam
    
    Differential Revision: https://code.vsanthanam.com/D397

--- Statio/App/Statio/Src/Features/Processor/ProcessorViewController.swift
+++ Statio/App/Statio/Src/Features/Processor/ProcessorViewController.swift
@@ -25,6 +25,16 @@ final class ProcessorViewController: ScopeViewController, ProcessorPresentable {
 
     // MARK: - UIViewController
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Processor"
+        let leadingItem = UIBarButtonItem(barButtonSystemItem: .close,
+                                          target: self,
+                                          action: #selector(didTapBack))
+        navigationItem.leftBarButtonItem = leadingItem
+        specializedView.backgroundColor = .systemBackground
+    }
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         analyticsManager.send(event: AnalyticsEvent.processor_vc_impression)
@@ -43,5 +53,6 @@ final class ProcessorViewController: ScopeViewController, ProcessorPresentable {
     @objc
     private func didTapBack() {
         analyticsManager.send(event: AnalyticsEvent.processor_vc_dismiss)
+        listener?.didTapBack()
     }
 }
---
+++ Statio/App/StatioTests/Features/Processor/ProcessorViewControllerSnapshotTests.swift
@@ -0,0 +1,23 @@
+//
+// Statio
+// Varun Santhanam
+//
+
+import FBSnapshotTestCase
+@testable import Statio
+
+final class ProcessorControllerSnapshotTests: SnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+    }
+
+    func test_processorViewController() {
+        let viewController = ProcessorViewController(analyticsManager: AnalyticsManagingMock())
+        viewController.loadView()
+        viewController.viewDidLoad()
+        FBSnapshotVerifyViewController(viewController)
+    }
+
+}
