93f775ca3e9fc67e622d31b52655b6cad88d31c8
Varun Santhanam <talkto@vsanthanam.com>
2021-10-16 11:50:50 -0700

    [Cellular] Add Snapshot Tests for `CellularViewController`
    Summary: add `CellularViewControllerSnapshotTests` + ref images
    
    Test Plan: n/a, tests only
    
    Differential Revision: https://code.vsanthanam.com/D419

---
+++ Statio/App/StatioSnapshotTests/Features/Cellular/CellularViewControllerSnapshotTests.swift
@@ -0,0 +1,26 @@
+//
+// Statio
+// Varun Santhanam
+//
+
+@testable import Analytics
+import Foundation
+@testable import ShortRibs
+@testable import Statio
+import XCTest
+
+final class CellularViewControllerSnapshotTests: SnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+    }
+
+    func test_cellularViewController() {
+        let viewController = CellularViewControler(analyticsManager: AnalyticsManagingMock())
+        viewController.loadView()
+        viewController.viewDidLoad()
+        FBSnapshotVerifyViewController(viewController)
+    }
+
+}
